7dd44bc74c64dd4b71f86fcbf790cb17a627eba8
Hudaibiya <123533030+Hudaibiya51@users.noreply.github.com>
2026-04-02 16:29:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";  // treats all the code as newer version
+//alert(2+2) // won't work because we are using nodejs not browser
+
+
+let name = "Huda"
+let age = 25
+let islearning = true
+
+// number  => 2 to  power 53
+// bigint
+//string => ""
+// boolean => true or false
+// null  => standalone value/ representation of empty value
+// undefined; => value not defined
+// symbol => to find uniqueness
+
+// object
+
+console.log(typeof null); // o/p => object
+console.log(typeof undefined); // O/p => undefined
