f6aeb65358b3d6313c050e3e9cf344f3ddee356a
Hudaibiya <123533030+Hudaibiya51@users.noreply.github.com>
2026-04-03 15:40:45 +0000

    operation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,29 +1,63 @@
-// conversion to num
-let score = "23"
-console.log(typeof score);
-let convertInNumber = Number(score) // string to number conversion
-console.log(typeof convertInNumber);
-console.log(convertInNumber);
+// // conversion to num
+// let score = "23"
+// console.log(typeof score);
+// let convertInNumber = Number(score) // string to number conversion
+// console.log(typeof convertInNumber);
+// console.log(convertInNumber);
 
 
-// if convert string like "45abd" , the O/p is NaN
-// it convert true it gives 1; for false = 0
-// if null = 0
-// if undefined =. NaN
+// // if convert string like "45abd" , the O/p is NaN
+// // it convert true it gives 1; for false = 0
+// // if null = 0
+// // if undefined = NaN
 
 
-// conversion to boolean
-let isLoggedIn = 1
-let toBoolean = Boolean(isLoggedIn)
-console.log(typeof toBoolean);
-console.log(toBoolean);
-// if 1 to boolean = true; 0 = false; "" = false; "huda"= true
+// // conversion to boolean
+// let isLoggedIn = 1
+// let toBoolean = Boolean(isLoggedIn)
+// console.log(typeof toBoolean);
+// console.log(toBoolean);
+// // if 1 to boolean = true; 0 = false; "" = false; "huda"= true
 
 
-//consersion to string
-let numberSin = 20
-let toString = String(numberSin)
-console.log(typeof toString);
-console.log(toString);
+// //consersion to string
+// let numberSin = 20
+// let toString = String(numberSin)
+// console.log(typeof toString);
+// console.log(toString);
+
+
+// Operationsss
+
+let num = 3
+let negvalue = -3
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/4);
+// console.log(2%4);
+
+let str1 = "Hey"
+let str2 = " Huda"
+// console.log(str1 + str2);
+
+
+//Postfix increment = the increment operator increments and returns the value before incrementing.
+let x = 3;
+const y = x++;
+// console.log(x);
+// console.log(y);
+
+
+
+//Prefix increment = the increment operator increments and returns the value after incrementing.
+let z = 3;
+const j = ++z;
+console.log(z);
+console.log(j);
+
 
 
