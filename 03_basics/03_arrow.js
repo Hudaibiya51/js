e4b9542f7d0ec3de54914cd50410eeea855b6e71
Hudaibiya <123533030+Hudaibiya51@users.noreply.github.com>
2026-05-04 16:01:11 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,10 +3,18 @@ const user = {
     price: 999,
     welcomeMsg: function(){
         console.log(`${this.username}, welcome to JS`);
+        console.log(this); // talks about the current context
+        
         
     }
 }
-user.welcomeMsg()// o/p=Huda, welcome to JS 
-user.username = "Imaz"
-user.welcomeMsg() // o/p =  Imaz, welcome to JS
+// user.welcomeMsg()// o/p=Huda, welcome to JS 
+// user.username = "Imaz"
+// user.welcomeMsg() // o/p =  Imaz, welcome to JS
 // so this keyword is used to refer to current context
+
+
+// console.log(this); // o/p = {} o/p is empty obj because when we execute command is node env there is not context in global
+// if we run the same thing in browser th o/p is window obj, because window is the global obj in browser
+
+
